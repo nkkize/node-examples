177bb951351a49e394d7ad168907e60f1cbf941b
rednerank <narender.kumar@talentica.com>
2017-12-12 15:37:18 +0530

    fix for function parametrs in anonymous function

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var rect = require("./rectangle");
 function solveRect(l, b) {
     console.log("solving for rectangle with l = " + l + " and b = " + b);
 
-    rect(l, b, (err, rectangle) => {
+    rect(l, b, (err, res) => {
         if (err) {
             console.log("ERROR : ", err.message)
         } else {
-            console.log("area of the rectangle of dimentions l = " + l + ", and b = " + b + " is " + rectangle.area());
-            console.log("area of the rectangle of dimentions l = " + l + ", and b = " + b + " is " + rectangle.perimeter())
+            console.log("area of the rectangle of dimentions l = " + l + ", and b = " + b + " is " + res.area());
+            console.log("perimeter of the rectangle of dimentions l = " + l + ", and b = " + b + " is " + res.perimeter())
         }
     })
 
--- rectangle.js
+++ rectangle.js
@@ -8,8 +8,8 @@ module.exports = (x, y, callback) => {
         setTimeout(() =>
                 callback(null,
                     {
-                        perimeter: (x, y) => (2 * (x + y)),
-                        area: (x, y) => (x * y)
+                        perimeter: () => (2 * (x + y)),
+                        area: () => (x * y)
                     }),
             2000);
 
