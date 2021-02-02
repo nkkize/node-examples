111cc9dd228e70676ffde31605c72d78d0b0ad25
Narender Kumar <nkk.snat@gmail.com>
2021-02-02 21:17:35 +0530

    Update and rename aegis.js to setcookie.js

--- aegis.js
+++
@@ -1,5 +0,0 @@
-function inlineScriptCookies() {
-    console.log("hello from script served from github aegis.js...");
-}
-
-inlineScriptCookies()
---
+++ setcookie.js
@@ -0,0 +1,5 @@
+function inlineScriptCookies() {
+    document.cookie = 'requested=aegis; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/; SameSite=None; Secure';
+}
+
+inlineScriptCookies()
