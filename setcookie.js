0d03616eb34b6576ccfbb1f2d42ac32e4a0cfd1b
Narender Kumar <nkk.snat@gmail.com>
2021-02-02 21:20:17 +0530

    Update setcookie.js

--- setcookie.js
+++ setcookie.js
@@ -1,5 +1,5 @@
 function inlineScriptCookies() {
-    document.cookie = 'requested=aegis; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/; SameSite=None; Secure';
+    document.cookie = 'github-analytics=aegis; expires=Sun, 1 Jan 2023 00:00:00 UTC; path=/; SameSite=None; Secure';
 }
 
 inlineScriptCookies()
